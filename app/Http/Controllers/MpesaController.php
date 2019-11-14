d254414244183231b9db37ecc07bf231315bfc4f
Lee Mbuuga <mbugua.dev@gmail.com>
2019-11-14 15:10:29 +0300

    Update Prodcut cert and SC token generator

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -17,7 +17,7 @@ class MpesaController extends Controller
             $Msisdn=$request->input('Msisdn');
             $ShortCode=env('MPESA_B2C_SHORTCODE', $request->input('ShortCode'));
 
-            $c2b=MpesaClient::requestC2B($ShortCode, $Amount, $Msisdn);
+            $c2b=MpesaClient::c2b($ShortCode, $Amount, $Msisdn);
             Log::info('c2b response >>'.$c2b);
             return \response()->json(
                 ['response'=>['data'=> json_decode($c2b)]
