e75256738a8d2d9d613f4ff18d155d1e48e5b9be
Lee Mbuuga <mbugua.dev@gmail.com>
2019-11-26 21:32:39 +0300

    Fix duplicate id on lodgements

--- database/migrations/2019_11_19_182520_lodgements.php
+++ database/migrations/2019_11_19_182520_lodgements.php
@@ -17,9 +17,9 @@ class Lodgements extends Migration
             $table->bigIncrements('id');
             $table->string('customer_name');
             $table->string('msisdn');
-            $table->string('mpesa_ref');
+            $table->string('mpesa_ref')->unique('mpesa_trans_id');
             $table->float('amount');
-            $table->dateTimeT('trans_time');
+            $table->dateTime('trans_time');
             $table->string('transaction_type');
             $table->string('business_shortcode');
             $table->string('bill_refnumber');
